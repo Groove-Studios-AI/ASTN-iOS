5c17ff1e9b669d9aa22891609e7d119047e1e9e3
Jovial Joel <myersoftsolutions@gmail.com>
2025-04-29 17:34:08 -0400

    Initial Project Commit

---
+++ ASTN/ASTN/ASTNApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASTNApp.swift
+//  ASTN
+//
+//  Created by Joel Myers on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ASTNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
